a67b3d1ec4bc1df5fde6e9e42171ca69cb3b0239
André Kirst <github@andrekirst.de>
2025-06-02 14:56:28 +0200

    fix: use it with copilot-code

--- n8n/01-secrets.bash
+++ n8n/01-secrets.bash
@@ -1,7 +1,4 @@
 microk8s kubectl create secret generic n8n-secrets \
-  --namespace n8n \
-  --from-literal=POSTGRES_PASSWORD='n8npass' \
-  --from-literal=POSTGRES_USER='n8n' \
-  --from-literal=POSTGRES_DB='n8n' \
-  --from-literal=N8N_BASIC_AUTH_USER='admin' \
-  --from-literal=N8N_BASIC_AUTH_PASSWORD='geheim'
+  --from-literal=N8N_BASIC_AUTH_USER=deinuser \
+  --from-literal=N8N_BASIC_AUTH_PASSWORD=deinpasswort \
+  -n n8n
