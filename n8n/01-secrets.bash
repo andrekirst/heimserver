65bc62aa3dc9b453480a189fa5686b9e7d590026
André Kirst <github@andrekirst.de>
2025-06-02 16:03:51 +0200

    feat: add postgres

--- n8n/01-secrets.bash
+++ n8n/01-secrets.bash
@@ -1,4 +1,7 @@
-microk8s kubectl create secret generic n8n-secrets \
+microk8s kubectl create secret generic postgres-secrets \
+  --namespace n8n \
+  --from-literal=POSTGRES_DB=n8ndb \
+  --from-literal=POSTGRES_USER=n8nuser \
+  --from-literal=POSTGRES_PASSWORD=n8npassword \
   --from-literal=N8N_BASIC_AUTH_USER=deinuser \
   --from-literal=N8N_BASIC_AUTH_PASSWORD=deinpasswort \
-  -n n8n
